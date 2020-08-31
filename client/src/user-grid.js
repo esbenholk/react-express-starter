1f2c52894d913d5d29215cca0de21f8177494e66
Esben Holk <esbenholknielsen@gmail.com>
2020-08-31 12:12:35 +0200

    created date calculator function for each user

--- client/src/user-grid.js
+++ client/src/user-grid.js
@@ -1,14 +1,17 @@
 import React from "react";
+import Calculator from './date-calculator.js';
 
 
 export class User extends React.Component {
-constructor(props){
-    super(props)
-    console.log(props.user.picture.url);
-}
+    constructor(props){
+        super(props)
+    }
     render() {
-      return <div>
-          <img src={this.props.user.picture.url}></img>
+      return <div className="user">
+          <img src={this.props.user.picture.url} alt="profile pic missing"></img>
+          <p>{this.props.user.name}</p>
+          <p>{this.props.user.personal.age}</p>
+          <Calculator user={this.props.user}/>
       </div>
     }
   }
